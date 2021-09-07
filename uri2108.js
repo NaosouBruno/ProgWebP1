4f13cab7ecd0aa42af620d375c92446f082def82
Bruno <44499918+NaosouBruno@users.noreply.github.com>
2021-09-07 17:29:37 -0400

    Add files via upload

---
+++ uri2108.js
@@ -0,0 +1,21 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let maior = "";
+let maiorTam = -1;
+let line = lines.shift();
+while(line != "0"){
+    
+    
+    let tamanhos = line.split(" ").map((w)=>{
+        if(maiorTam <= w.length){
+            maior = w;
+            maiorTam = w.length
+        }
+        return w.length;
+    })
+    console.log(tamanhos.join("-"));
+   
+    let line = lines.shift();
+}
+console.log(`\nThe biggest word: chondrocraniums: ${maior}`)
